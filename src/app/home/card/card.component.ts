4e747f19482d18a9d151122e2b26c287d9f665a2
malethik <quintiliani.d@gmail.com>
2024-03-23 23:53:51 +0100

    updating visual files

--- src/app/home/card/card.component.ts
+++ src/app/home/card/card.component.ts
@@ -8,20 +8,26 @@ import { Pokemon } from '../../core/model/pokemon';
   template: `
     <div class="pokemon">
       <p></p>
-      <img
-        src="../assets/heart.png"
-        alt=""
-        class="hearts"
-        height="10"
-        width="10"
-      />
+
       <p>{{ pokemonInfo.name }}</p>
-      <p>Type: {{ pokemonInfo.id }}</p>
-      <img src="" alt="" />
+      <p>Weight: {{ pokemonInfo.weight }} Gr</p>
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/{{
+          pokemonInfo.id
+        }}.png"
+        alt="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/{{
+          pokemonInfo.id
+        }}.png"
+        width="100"
+        height="100"
+      />
     </div>
   `,
   styles: ``,
 })
 export class CardComponent {
-  @Input() pokemonInfo!: any;
+  @Input() pokemonInfo!: Pokemon;
 }
+/*  https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1000.png */
+
+/* https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1000.png */
