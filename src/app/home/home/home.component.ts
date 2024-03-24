06efa698c638e21dfaaf444518a152197a9c768a
Daniele <quintiliani.d@gmail.com>
2024-03-24 18:58:40 +0100

    Improved button funcionally

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -7,22 +7,37 @@ import { Pokemon } from '../../core/model/pokemon';
   selector: 'app-home',
   standalone: true,
   imports: [CardComponent],
-  template: `<div class="main">
-    @for (item of pokemonList ; track $index) {
-    <app-card [pokemonInfo]="item" />
-    }
-  </div>`,
+  template: ` <div class="navBar">
+      <button type="button" class="previus">PREVIUS</button>
+      <h2>Pokemon list:</h2>
+      <button type="button" class="next" (click)="loadNextPage()">NEXT</button>
+    </div>
+
+    <div class="main">
+      @for (item of pokemonList ; track $index) {
+      <app-card [pokemonInfo]="item" />
+      }
+    </div>`,
   styles: ``,
 })
 export class HomeComponent implements OnInit {
   pokemonList: Pokemon[] = [];
+  page = 0;
   private Apiservice = inject(ApiService);
   constructor(private apiService: ApiService) {
     this.Apiservice.getData();
   }
+  loadNextPage() {
+    this.page += 20;
+    this.ngOnInit();
+  }
+  loadPreviusPage() {
+    this.page -= 20;
+    this.ngOnInit();
+  }
 
   ngOnInit(): void {
-    this.apiService.getPokemonList().subscribe((pokemonList) => {
+    this.apiService.getPokemonList(this.page, 20).subscribe((pokemonList) => {
       this.apiService
         .fetchDetailedPokemonData(pokemonList)
         .subscribe((detailedPokemonList) => {
